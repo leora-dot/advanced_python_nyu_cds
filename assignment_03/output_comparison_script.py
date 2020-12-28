8f25b8d9f6fe93e193f34e50f77473cfd6d13dea
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-12-27 19:06:22 -0500

    cleaning & instructions

--- assignment_03/output_comparison_script.py
+++ assignment_03/output_comparison_script.py
@@ -1,9 +1,25 @@
-def is_identical(file_01, file_02):
-    list_01, list_02 = open(file_01).readlines(  ), open(file_02).readlines(  )
+"""
+Script to confirm that changes to nbody scripts have not impacted its output
 
+Testing Instructions:
+    #Run both versions of the program, piping results into txt files
+    #Enter numerical values associated with each txt file into is_identical
+    #Function will return true if output matches & false if it does not
+
+"""
+
+def is_identical(iteration_num_1, iteration_num_2):
+
+    filename_dict = {
+        0: "output_00_original.txt",
+        1: "output_01_functions.txt",
+        2: "output_02_variables.txt",
+        3: "output_03_looping.txt",
+        4: "output_04_built_in.txt",
+        5: "output_05_total_changes.txt"}
+
+    filename_01, filename_02 = filename_dict[iteration_num_1], filename_dict[iteration_num_2]
+    list_01, list_02 = open(filename_01).readlines(  ), open(filename_02).readlines(  )
     return list_01 == list_02
 
-#"output_00_original.txt"
-#"output_01_functions.txt"
-
-print(is_identical("output_00_original.txt", "output_01_functions.txt"))
+print(is_identical(0, 2))
